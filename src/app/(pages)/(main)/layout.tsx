9aedf23b1ae88f876b5521ca30fcc8482040183d
Nathan <nathanferreira00@hotmail.com>
2024-05-23 14:27:04 -0300

    chore: fix build error

--- src/app/(pages)/(main)/layout.tsx
+++ src/app/(pages)/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from '@/app/components/header'
+import SuspenseWrapper from '@/app/components/suspense-wrapper'
 import { type ReactNode } from 'react'
 
 export default function AppLayout({ children }: { children: ReactNode }) {
@@ -6,7 +7,7 @@ export default function AppLayout({ children }: { children: ReactNode }) {
     <div className="flex min-h-screen flex-col">
       <Header />
       <div className="mx-auto flex w-full max-w-[1600px] flex-1 p-8 pt-6">
-        {children}
+        <SuspenseWrapper>{children}</SuspenseWrapper>
       </div>
     </div>
   )
